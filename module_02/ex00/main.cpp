fde39fdbbed7a78fa86ded0d8148e0906bacec7f
mzaraa <mzaraa@student.42nice.fr>
2022-10-22 17:59:24 +0200

    new_module

---
+++ module_02/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<cctype>
+
+
+int	main(int ac, char **av)
+{
+	std::ostringstream	os;
+	std::string			str;
+
+	if(ac > 1)
+	{
+		while (*(++av))
+			os << *av;
+		str = os.str();
+		for(std::string::iterator it = str.begin(); it != str.end(); ++it)
+			*it = std::toupper(*it);
+		std::cout << str << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
